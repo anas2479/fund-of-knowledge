d83687990d42dd3279664c7676d0a0b6b90f954a
Anas Mohamed <amohamed2479@gmail.com>
2021-12-02 06:51:53 -0600

    fix up footer

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,12 +1,13 @@
-<div class="max-w-[1200px] p-4 mx-auto md:flex mb-12">
-    <div class="flex-1 mb-7">
-        <img src="./assets/illustrations/hey.svg">
-    <p class="">
-        Like many of my works, this site is open source. This means you can view the code that makes this website work if it intrests you.
-        check it out.
-    </p>
+<div class="max-w-[1200px] p-4 mx-auto md:flex mb-12 gap-4">
+    <div class="mb-7">
+        <img src="https://anasweb.site/assets/face.svg" alt="anas mohamed" width="100px">
+    
     </div>
-    <div class="flex-1">
+    <div class="max-w-[400px]">
+        <p class="mb-8">
+            Like many of my works, this site is open source. This means you can view the code that makes this website work if it intrests you.
+            check it out.
+        </p>
         <a class="p-4 px-10 border-2 border-black text-black hover:text-white hover:bg-black font-medium rounded-sm" href="https://github.com/anas2479/fund-of-knowledge">View the Code</a>
     </div>
 </div>
